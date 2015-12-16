0731a338fa443eb0bbf558810cae4daad6f51aa1
dbcjckyee <dbcjckyee@gmail.com>
2015-12-15 20:27:19 -0800

    reorganize code so that youtube player code is up top

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,11 +14,30 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('content', {
+  events: {
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerStateChange(event) {
+  if (event.data === 0) {
+    nextVideo();
+  }
+}
+
 var vidcount = 0;
 var vidarray = [];
 
 function nextVideo() {
- var checknext = new Promise(function(resolve,reject){
+  var checknext = new Promise(function(resolve,reject){
   if(vidcount == vidarray.length-1){
     $.ajax({
       method: "POST",
@@ -95,33 +114,4 @@ $(document).ready(function(){
     }
   })
 })
- // 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('content', {
-  events: {
-      'onStateChange': onPlayerStateChange
-    }
-  });
-}
-
-// 4. The API will call this function when the video player is ready.
-
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-function onPlayerStateChange(event) {
-  if (event.data === 0) {
-    nextVideo();
-  }
-}
 
