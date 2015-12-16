c66d7da952b6f8acc21554a5e58053e44dd7d999
dbcjckyee <dbcjckyee@gmail.com>
2015-12-15 22:33:45 -0800

    fix environment variable designation for soundcloud in rails

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -1,31 +1,12 @@
 class MusicController < ApplicationController
-
   def create
-    client = Soundcloud.new(:client_id => '5eab25892177da647ada924c74038554')
-
-  # find all tracks with the genre 'punk' that have a tempo greater than 120 bpm.
-    tracks = client.get('/tracks', :q => 'PITBULL', :limit => 200, :filter => 'public'
-    )
-    # tracks.each do |track|
-    #   p track[:permalink_url]
-
-    # end
+    client = Soundcloud.new(:client_id => ENV["SCID"])
+    tracks = client.get('/tracks', :q => 'PITBULL', :limit => 200, :filter => 'public')
     p tracks.shuffle[0][:permalink_url]
-    # p tracks.shuffle[0]
-    # embedurl = client.get('/oembed', :url => tracks.shuffle[0][:permalink_url])
-    # p embedurl
     if request.xhr?
       render :json => {
                         :music => tracks.shuffle[0][:permalink_url]
                       }
     end
-
-        # get a tracks oembed data
-    # track_url = 'http://soundcloud.com/forss/flickermood'
-    # embed_info = client.get('/oembed', :url => track_url)
-
-    # # print the html for the player widget
-    # puts embed_info['html']
   end
-
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    # vidarray = main[:vid]
-    # @video = "http://www.youtube.com/embed/#{vidarray[20]}"
-    # p @video
   end
 end
