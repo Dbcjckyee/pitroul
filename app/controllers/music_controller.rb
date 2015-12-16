a8b457abd1e4843b07083cc6e3fbd6f89061aaed
dbcjckyee <dbcjckyee@gmail.com>
2015-12-15 22:50:19 -0800

    clean out api comments from rails sections, clean up formatting in rails sections

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -2,11 +2,10 @@ class MusicController < ApplicationController
   def create
     client = Soundcloud.new(:client_id => ENV["SCID"])
     tracks = client.get('/tracks', :q => 'PITBULL', :limit => 200, :filter => 'public')
-    p tracks.shuffle[0][:permalink_url]
     if request.xhr?
       render :json => {
-                        :music => tracks.shuffle[0][:permalink_url]
-                      }
+        :music => tracks.shuffle[0][:permalink_url]
+      }
     end
   end
 end
